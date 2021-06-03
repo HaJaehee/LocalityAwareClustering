76e963c735d06d1f53093f4b825e1eea0926c0bd
J.Ha <jaehee.ha@kaist.ac.kr>
2021-06-03 21:42:54 +0900

    First approach implementation is done.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@
  Created this module.
  Implemented 2-mean clustering algorithm for edge nodes.
 
+ Rev. history : 2021-06-03
+ Version : 1.0.0
+ First approach implementation is done.
+ Modifier : Jaehee ha (jaehee.ha@kaist.ac.kr)
+
 """
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -60,7 +65,7 @@ def main():
 
     level=int(5)
 
-    #kmeans = KMeans(n_clusters=2)
+    #kmeans = KMeans(n_clusters=16)
     #kmeans.fit(X)
     #print(kmeans.cluster_centers_)
     #print(kmeans.labels_)
@@ -72,7 +77,7 @@ def main():
     two_means_cluster(X, level)
 
     global __result_list__
-    print (__result_list__)
+    #print (__result_list__)
 
     coordinate_list_with_name_n_label=[]
     idx_i=0
@@ -80,13 +85,20 @@ def main():
         idx_j=0
         for j in coordinate_list_with_name:
             if j[1] == i[0] and j[2] == i[1]:
-                coordinate_list_with_name_n_label.append([j[0], j[1], j[2], bin(i[2]).split("b")[1].zfill(5)])
+                coordinate_list_with_name_n_label.append([j[0], j[1], j[2], bin(i[2]).split("b")[1].zfill(5), i[3]])
                 del coordinate_list_with_name[idx_j]
             idx_j += 1
         idx_i += 1
 
     coordinate_list_with_name_n_label.sort(key=lambda x:x[1])
-    print(coordinate_list_with_name_n_label)
+
+    #print("\n\n")
+    #print(coordinate_list_with_name_n_label)
+
+    node_list_file = open("korea-100-router-node-list.txt", 'w')
+    for i in coordinate_list_with_name_n_label :
+        node_list_file.write(str(i[3])+","+bin(int(i[0].split("n")[1])).split("b")[1].zfill(10)+","+str(i[0])+","+str(i[1])+","+str(i[2])+","+str(i[4])+","+"\n")
+    node_list_file.close()
 
     imn_file = open("korea-100-router.imn", 'r')
 
@@ -96,11 +108,32 @@ def main():
         line = imn_file.readline()
         if not line: break
 
-        idx_i = 0
         for i in coordinate_list_with_name_n_label:
-            if re.search(r'\b'+i[0]+r'\b', line):
-                line = line.replace(i[0], i[3]+i[0])
-                idx_i += 1
+            if "nodes" in line :
+                line_split = line.split(" ")
+                if str("{"+i[0]) == line_split[5] :
+                    line_split[5] = "{"+i[3]+bin(int(i[0].split("n")[1])).split("b")[1].zfill(10)
+                if str(i[0]+"}\n") == line_split[6] :
+                    line_split[6] = i[3]+bin(int(i[0].split("n")[1])).split("b")[1].zfill(10)+"}\n"
+                line = "    "+line_split[4]+" "+line_split[5]+" "+line_split[6]
+
+            elif "node" in line :
+                line_split = line.split(" ")
+                if i[0] == line_split[1] :
+                    line_split[1] = i[3]+bin(int(i[0].split("n")[1])).split("b")[1].zfill(10)
+                line = line_split[0]+" "+line_split[1]+" "+line_split[2]
+
+            elif "hostname" in line:
+                line_substr = line[10:]
+                if str(i[0]+"\n") == line_substr :
+                    line_substr = i[3]+bin(int(i[0].split("n")[1])).split("b")[1].zfill(10)+"\n"
+                line = "\thostname "+line_substr
+
+            elif "interface-peer" in line:
+                line_split = line.split(" ")
+                if str(i[0]+"}\n") == line_split[6]:
+                    line_split[6] = i[3]+bin(int(i[0].split("n")[1])).split("b")[1].zfill(10)+"}\n"
+                line = "    "+line_split[4]+" "+line_split[5]+" "+line_split[6]
         imn_file2.write(line)
     imn_file.close()
     imn_file2.close()
@@ -117,29 +150,38 @@ def two_means_cluster(array_x, level):
     a = []
     b = []
     idx = 0
-    #TODO
     idx_minimum_a = 0
     minimum_dist_a = 0
-    idx_minimum_temp_a = 0
     minimum_dist_temp_a = 10000000
     idx_minimum_b = 0
     minimum_dist_b = 0
-    idx_minimum_temp_b = 0
     minimum_dist_temp_b = 10000000
     for i in array_x:
         if kmeans.labels_[idx] == 0:
             a.append(i)
             if level == 1:
-                minimum_dist_a = (i[0]-kmeans.cluster_centers_[0][0] ** 2 ) + (i[1]-kmeans.cluster_centers_[0][1] ** 2)
+                minimum_dist_a = ((i[0]-kmeans.cluster_centers_[0][0]) ** 2) + ((i[1]-kmeans.cluster_centers_[0][1]) ** 2)
                 if minimum_dist_a < minimum_dist_temp_a :
-                    idx_minimum_a = idx_minimum_temp_a
+                    if len(__result_list__) > 0 :
+                        __result_list__[idx_minimum_a][3] = ''
                     __result_list__.append([i[0], i[1], __result_label__, 'centroid'])
+                    idx_minimum_a = len(__result_list__) - 1
+                    minimum_dist_temp_a = minimum_dist_a
                 else :
-                    __result_list__.append([i[0], i[1], __result_label__])
+                    __result_list__.append([i[0], i[1], __result_label__, ''])
+
         elif kmeans.labels_[idx] == 1:
             b.append(i)
             if level == 1:
-                __result_list__.append([i[0], i[1], __result_label__+1])
+                minimum_dist_b = ((i[0]-kmeans.cluster_centers_[1][0]) ** 2) + ((i[1]-kmeans.cluster_centers_[1][1]) ** 2)
+                if minimum_dist_b < minimum_dist_temp_b :
+                    if len(__result_list__) > 0 :
+                        __result_list__[idx_minimum_b][3] = ''
+                    __result_list__.append([i[0], i[1], __result_label__, 'centroid'])
+                    idx_minimum_b = len(__result_list__) - 1
+                    minimum_dist_temp_b = minimum_dist_b
+                else :
+                    __result_list__.append([i[0], i[1], __result_label__+1, ''])
         idx += 1
     level -= 1
     np_a = np.array(a[:])
@@ -147,7 +189,7 @@ def two_means_cluster(array_x, level):
     if level == 0:
         #print (np_a)
         #print (np_b)
-        print (kmeans.cluster_centers_)
+        #print (kmeans.cluster_centers_)
         __result_label__ += 1
 
     two_means_cluster(np_a, level)
