dde19435225e0c614fb864ae629027e6d1279959
J.Ha <jaehee.ha@kaist.ac.kr>
2021-06-07 00:17:05 +0900

    Implementation is ongoing

--- mod_of_locality_id/main.py
+++ mod_of_locality_id/main.py
@@ -18,11 +18,12 @@ def main():
     id_replacement_file = open("../korea-100-router-clustered-id-rep.imn", 'w')
     centroid_list_file = open("../korea-100-router-centroid-list.txt", 'r')
     centroid_list = []
-
+    centroid_ip_list = []
     while True :
         line = centroid_list_file.readline()
         if not line : break
         centroid_list.append(line.split(","))
+        centroid_ip_list.append(centroid_list[len(centroid_list)-1][2])
     centroid_list_file.close()
 
     path_dir = "./latencies"
@@ -32,18 +33,23 @@ def main():
         for centroid in centroid_list :
             if centroid[0]+centroid[1] == file.replace("-latencies.txt", ""): continue
         latency_file = open(path_dir+"/"+file, 'r')
+        lowest_latency = 100.0
+        lowest_latency_ip = str()
         while True :
             line = latency_file.readline()
             if not line : break
-            lowest_latency = 100
-            lowest_latency_ip = str()
             if "icmp_seq=5" in line :
                 line_split = line.split(" ")
-                if lowest_latency > line_split[6].replace("time=", ""):
-                    lowest_latency = line_split[6].replace("time=", "")
+                if lowest_latency > float(line_split[6].replace("time=", "")):
+                    lowest_latency = float(line_split[6].replace("time=", ""))
                     lowest_latency_ip = line_split[3].replace(":", "")
         latency_file.close()
-        
+        centroid_list_idx = centroid_ip_list.index(lowest_latency_ip)
+        new_locality_id = centroid_list[centroid_list_idx][0]
+        print("old "+file.replace("-latencies.txt", "")[0:5]+" new "+new_locality_id)
+
+    id_replacement_file.close()
+    clustered_file.close()
 
 
 
